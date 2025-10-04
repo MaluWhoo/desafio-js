b2268b6ad425a44857bb7bc3ad902082fb8e0631
Maria Luiza Lima <luizagamesl90@gmail.com>
2025-10-04 14:40:03 -0300

    update js

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 alert("Desafios de JS!");
 
 // Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
-let contador = 1;
-while (contador <= 10){
-    console.log(`${contador}`);
-}
+// let contador = 1;
+// while (contador <= 10){
+//     console.log( contador );
+//     contador++;
+// }
 
-// 
+
+// Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+// let contador = 10;
+// while (contador >= 0){
+//     console.log(contador);
+//     contador--;
+// }
+
+
+//Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.
+
+// let numero = prompt("Digite um número para iniciar a contagem refressiva:");
+// while (numero >= 0){
+//     console.log(numero);
+//     numero--;
+// }
+
+
+// Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.
+
+let contador = 0;
+let numero = prompt("Digite um número para iniciar a contagem progressiva:");
+
+while(contador <= numero ){
+    console.log(contador);
+    contador++;
+}
